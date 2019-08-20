846dd7b757803346f546a5a9d4f82e5b3df6698b
Dalton Sconce <dscone@protonmail.com>
2019-08-19 21:34:24 -0400

    asdf

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,22 @@ pub struct Arguments {
 }
 
 impl Arguments {
-    pub fn
+    pub fn new(args: &[String]) -> Arguments {
+        if is_flag_valid(args[1].clone) {
+            let encode_or_decode = args[1].clone;
+            let key = args[2].clone().parse()::<i8>().expect("Error, not a number");
+            let message = args[3];
+            Arguments { encode_or_decode,key,message }
+        }
+        else {
+            print_help();
+        }
+    }
 }
 
+pub fn print_help() {
+    println!("Unknown command\n")
+}
 pub fn is_flag_valid(flag: String) -> bool {
     let mut is_valid: bool;
     match flag {
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,4 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 }
