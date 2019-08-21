b5c6b54d4b9f827c07a48c845e7323dfaac62e34
Dalton Sconce <dscone@protonmail.com>
2019-08-21 00:41:47 -0400

    added some comments, changed key type from i8 to usize

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let collection = caesar_cipher::Arguments::new(&args);
-    collection.print();
+    //collection.print();
     let secret = collection.translate();
-    println!("{}", secret);
+    println!("The encoded message is '{}'", secret);
 }
