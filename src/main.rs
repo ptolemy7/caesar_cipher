53725d791b48036a62484fb8b3340953558ed997
Dalton Sconce <dscone@protonmail.com>
2019-08-25 10:14:09 -0400

    Fixed main.rs, added comments

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use caesar_cipher as cipher;
 use std::env;
 
 fn main() {
+    //Collect the input stream
     let args: Vec<String> = env::args().collect();
-    let collection = cipher::Arguments::new(&args);
-    //collection.print();
+    //Pass the args into new
+    let collection = cipher::Arguments::new(&args).unwrap();
+    //Run the translation(the rest of the logic is in lib.rs)
     let secret = collection.run();
     println!(
         "The message is:
